330bf15913c6a03488b34e1e5a07df4efcc54a00
Syed Musa <musabinsyed@gmail.com>
2023-08-19 10:35:44 +0600

    added a calculation section

--- scripts/area-calculate.js
+++ scripts/area-calculate.js
@@ -8,4 +8,29 @@ function calculatetriangleArea(){
     const heightValueText = hieghtField.value;
     const height = parseFloat(heightValueText);
     console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area);
+
+
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+}
+
+
+function calculateRectangleArea(){
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    console.log(width);
+
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    console.log(length);
+
+    const area = width * length;
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
